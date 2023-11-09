856e1d419018c2dc85823bb17045785e900d82e0
Maekin <74455990+mekintown@users.noreply.github.com>
2023-11-09 23:48:25 +0700

    feat: Add next-auth

---
+++ src/libs/userLogin.ts
@@ -0,0 +1,20 @@
+export default async function userLogIn(
+  userEmail: string,
+  userPassword: string
+) {
+  const response = await fetch("http://localhost:5001/api/v1/auth/login", {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify({
+      email: userEmail,
+      password: userPassword,
+    }),
+  });
+
+  if (!response.ok) {
+    throw new Error("Failed to fetch cars");
+  }
+  return await response.json();
+}
